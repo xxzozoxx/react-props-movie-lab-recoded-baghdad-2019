35c87b3630a304b269a2daa7c5ca9ef831646bd6
zozo <zahraa.jalaa99@gmail.com>
2019-11-18 08:55:31 +0300

    updated passing props

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,9 +6,13 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    return(
+      movieData.map(data => <MovieCard {...data}/>)
+    )
   }
 
   render() {
+  
     return (
       <div id="movie-showcase">
         {this.generateMovieCards()}
